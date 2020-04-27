68702165f30238f4f91688f0106589c4b567febd
Ray Alva <rayalva407@gmail.com>
2020-04-27 19:16:20 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -15,6 +15,14 @@ ActiveRecord::Base.establish_connection(
   )
 sql = <<SQL-
   CREATE TABLE IF NOT EXISTS artists (
-  
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
   )
+  SQL
+  
+  ActiveRecord::Base.connection.execute(sql)
+  
 require_relative "../artist.rb"
