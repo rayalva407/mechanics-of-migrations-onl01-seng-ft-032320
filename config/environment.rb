39a82dd6922746e0cb5085c84e34add4ff346d96
Ray Alva <rayalva407@gmail.com>
2020-04-27 19:39:40 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -13,15 +13,6 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artist.sqlite"
   )
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
   
   ActiveRecord::Base.connection.execute(sql)
   
